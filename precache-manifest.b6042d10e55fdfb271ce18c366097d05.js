8873b3b07081ed6cc881612e2d96d048c2a16345
raghu-svit <raghavenderk421@gmail.com>
2021-01-26 07:01:12 +0530

    Updates

---
+++ precache-manifest.b6042d10e55fdfb271ce18c366097d05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6356532d838b8174cc6ed9e76f4c4c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "113682a46a7538bdca12",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d0f04a88bcb0ca1d5154",
+    "url": "/my-app/static/js/2.f682b829.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.f682b829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113682a46a7538bdca12",
+    "url": "/my-app/static/js/main.bc1aa705.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
